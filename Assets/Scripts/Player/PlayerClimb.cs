5f076768d37c1ce94f9f5bd5727135a59ab28397
EmonS <fnhaan@gmail.com>
2024-10-22 16:55:59 +0700

    UpdateClimb

--- Assets/Scripts/Player/PlayerClimb.cs
+++ Assets/Scripts/Player/PlayerClimb.cs
@@ -10,22 +10,22 @@ public class PlayerClimb : MonoBehaviour
     [SerializeField] private Vector2 wallCheckSize; 
     [SerializeField] private Vector2 wallCheckOffset;
 
-    [SerializeField] private Vector2 offset1; 
-    [SerializeField] private Vector2 offset2; 
-    private Vector2 climbBegunPosition; 
-    private Vector2 climbOverPosition; 
+    [SerializeField] private Vector2 offset1; // Offset to the starting position of the climb
+    [SerializeField] private Vector2 offset2; // Offset to the end position of the climb
+    private Vector2 climbBegunPosition; // Position where the climb begins
+    private Vector2 climbOverPosition; // Position where the climb ends
     public LayerMask groundLayer;
     private bool isWallDetected;
     public bool ledgeDetected;
     private bool canGrabLedge = true;
     public bool canClimb;
 
-    private bool isClimbing = false;
+    private bool isClimbing = false; // State to track climbing
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>(); 
+        animator = GetComponent<Animator>(); // Animator reference
     }
 
     void Update()
@@ -46,12 +46,21 @@ public class PlayerClimb : MonoBehaviour
 
             Vector2 ledgePosition = GetComponentInChildren<LedgeDetection>().transform.position;
 
-            climbBegunPosition = ledgePosition + offset1;
-            climbOverPosition = ledgePosition + offset2;
+            // Adjust climb positions based on the player's facing direction
+            if (transform.localScale.x < 0) // If the player is facing left
+            {
+                climbBegunPosition = ledgePosition + new Vector2(-offset1.x, offset1.y);
+                climbOverPosition = ledgePosition + new Vector2(-offset2.x, offset2.y);
+            }
+            else // If the player is facing right
+            {
+                climbBegunPosition = ledgePosition + offset1;
+                climbOverPosition = ledgePosition + offset2;
+            }
 
             canClimb = true;
-            isClimbing = true; 
-            animator.SetBool("canClimb", true); 
+            isClimbing = true; // Start climbing
+            animator.SetBool("canClimb", true); // Trigger climbing animation
 
             rb.velocity = Vector2.zero;
             rb.isKinematic = true;
@@ -69,9 +78,9 @@ public class PlayerClimb : MonoBehaviour
         transform.position = climbOverPosition;
 
         canClimb = false;
-        isClimbing = false; 
-        animator.SetBool("canClimb", false); 
-        rb.isKinematic = false; 
+        isClimbing = false; // Reset climbing state
+        animator.SetBool("canClimb", false); // End climbing animation
+        rb.isKinematic = false; // Re-enable player control
 
         AllowLedgeGrab();
     }
