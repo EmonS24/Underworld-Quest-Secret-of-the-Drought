609618a022ad901d0fa15b918d2cb5f37530856d
EmonS <fnhaan@gmail.com>
2024-11-04 20:20:42 +0700

    Lever Door Mechanic

---
+++ Assets/Scripts/Environment/Lever.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public DoorController door;
+    public Sprite leverActiveSprite;
+    public Sprite leverInactiveSprite;
+    private SpriteRenderer spriteRenderer;
+    private bool isActive = false;
+    private bool isPlayerInTrigger = false;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = leverInactiveSprite;
+    }
+
+    private void Update()
+    {
+        if (isPlayerInTrigger && Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleLever();
+        }
+    }
+
+    private void ToggleLever()
+    {
+        isActive = !isActive;
+
+        if (isActive)
+        {
+            spriteRenderer.sprite = leverActiveSprite;
+            door.OpenDoor();
+        }
+        else
+        {
+            spriteRenderer.sprite = leverInactiveSprite;
+            door.CloseDoor();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+}
