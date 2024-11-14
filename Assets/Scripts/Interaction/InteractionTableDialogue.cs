6c0fbb3a7532bdc83f9e9b4ea22baedee148a897
Raudiah RFH <raudiahr.fh@gmail.com>
2024-11-14 09:15:51 +0700

    Pick for Interaction NPC

---
+++ Assets/Scripts/Interaction/InteractionTableDialogue.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class InteractionTableDialogue : MonoBehaviour
+{
+    [SerializeField] private GameObject ui;
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log(message:"Player Interact!");
+        ui.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(message:"Object Terdeteksi");
+        ui.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        ui.SetActive(false);
+    }
+}
