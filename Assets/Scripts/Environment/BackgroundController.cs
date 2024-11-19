9792816c97dbbc73f247f488f2167cd11eaebf88
EmonS <fnhaan@gmail.com>
2024-11-19 16:06:21 +0700

    cloud movement

--- Assets/Scripts/Environment/BackgroundController.cs
+++ Assets/Scripts/Environment/BackgroundController.cs
@@ -7,17 +7,29 @@ public class BackgroundController : MonoBehaviour
     private float startPos, length;
     public GameObject cam;
     public float parallaxEffect;
+    public float cloudSpeed;
+
+    private bool isCloud;
+    private float cloudOffset = 0f;
+
     void Start()
     {
         startPos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
+
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        if (renderer != null && renderer.sortingLayerName == "Cloud")
+        {
+            isCloud = true;
+        }
     }
+
     void FixedUpdate()
     {
         float distance = cam.transform.position.x * parallaxEffect;
         float movement = cam.transform.position.x * (1 - parallaxEffect);
 
-        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startPos + distance + cloudOffset, transform.position.y, transform.position.z);
 
         if (movement > startPos + length)
         {
@@ -26,6 +38,20 @@ public class BackgroundController : MonoBehaviour
         else if (movement < startPos - length)
         {
             startPos -= length;
+        }
+        
+        if (isCloud)
+        {
+            cloudOffset += Time.deltaTime * cloudSpeed;
+
+            if (cloudOffset > length)
+            {
+                cloudOffset -= length;
+            }
+            else if (cloudOffset < -length)
+            {
+                cloudOffset += length;
+            }
         }
     }
 }
