77db313a1e859944b19c8907d97b5d4229ba37bf
EmonS <fnhaan@gmail.com>
2024-11-27 03:37:05 +0700

    Pause Menu

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -6,21 +6,36 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    AudioManager audioManager;
 
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
     public void Pause()
     {
+        audioManager.PlaySFX(audioManager.buttonClick);
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
     }
 
     public void Home()
     {
+        audioManager.PlaySFX(audioManager.buttonClick);
         SceneManager.LoadSceneAsync(0);
     }
 
     public void Continue()
     {
+        audioManager.PlaySFX(audioManager.buttonClick);
         pauseMenu.SetActive(false);
         Time.timeScale = 1;   
+    }
+
+    public void QuitGame()
+    {
+        audioManager.PlaySFX(audioManager.buttonClick);
+        Debug.Log("Quit!");
+        Application.Quit();
     }
 }
